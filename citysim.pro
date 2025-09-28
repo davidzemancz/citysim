22d234e8622a20bfe1460955d06ddec50b90f06b
David Zeman <david.zeman@m-line.cz>
2025-09-28 20:35:16 +0200

    qt init

---
+++ citysim.pro
@@ -0,0 +1,20 @@
+QT += core widgets gui
+
+CONFIG += c++17
+
+TARGET = citysim
+TEMPLATE = app
+
+SOURCES += main.cpp
+
+HEADERS +=
+
+# The following define makes your compiler emit warnings if you use
+# any feature of Qt which has been marked as deprecated (the exact warnings
+# depend on your compiler). Please consult the documentation of the
+# deprecated API in order to know how to port your code away from it.
+DEFINES += QT_DEPRECATED_WARNINGS
+
+# You can also make your code fail to compile if you use deprecated APIs.
+# In order to do so, uncomment the following line.
+# DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
+#include <QApplication>
+#include <QMainWindow>
+#include <QWidget>
+#include <QPainter>
+#include <QTimer>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-// clang++ -o main main.cpp && ./main
-
 const int MAP_WIDTH = 20;
 const int MAP_HEIGHT = 20;
+const int CELL_SIZE = 25; // Size of each cell in pixels
 
 enum MapPointType {
     EMPTY,
@@ -31,23 +35,104 @@ class MapPoint{
         MapPoint(int x, int y, MapPointType type, Direction direction) : x(x), y(y), type(type), direction(direction) {}
 };
 
-vector<vector<MapPoint>> createEmptyMap();
-void createRoads(vector<vector<MapPoint>>& map);
-void printMap(const vector<vector<MapPoint>>& map);
+class MapWidget : public QWidget {
+    Q_OBJECT
 
-int main() {
-    cout << "Creating map..." << endl;
-    auto map = createEmptyMap();
-    cout << "Map created successfully!" << endl;
-    cout << "Map size: " << MAP_WIDTH << "x" << MAP_HEIGHT << endl;
-    cout << "Adding roads..." << endl;
-    createRoads(map);
-    cout << "Roads added." << endl;
-    printMap(map);
-    return 0;
+private:
+    vector<vector<MapPoint>> map;
+
+public:
+    MapWidget(QWidget *parent = nullptr) : QWidget(parent) {
+        setFixedSize(MAP_WIDTH * CELL_SIZE, MAP_HEIGHT * CELL_SIZE);
+        setWindowTitle("City Simulation");
+        
+        // Initialize the map
+        map = createEmptyMap();
+        createRoads(map);
+        
+        cout << "Map created with Qt window!" << endl;
+    }
+
+protected:
+    void paintEvent(QPaintEvent *event) override {
+        QPainter painter(this);
+        painter.setRenderHint(QPainter::Antialiasing);
+
+        for (int y = 0; y < MAP_HEIGHT; y++) {
+            for (int x = 0; x < MAP_WIDTH; x++) {
+                QRect rect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+                
+                // Set colors based on map point type
+                if (map[x][y].type == EMPTY) {
+                    painter.fillRect(rect, QColor(34, 139, 34)); // Forest green for empty
+                } else if (map[x][y].type == ROAD) {
+                    painter.fillRect(rect, QColor(64, 64, 64)); // Dark gray for roads
+                    
+                    // Draw direction arrows
+                    painter.setPen(QPen(Qt::white, 2));
+                    QPoint center = rect.center();
+                    
+                    switch (map[x][y].direction) {
+                        case NORTH:
+                            painter.drawLine(center, center + QPoint(0, -8));
+                            painter.drawLine(center + QPoint(0, -8), center + QPoint(-3, -5));
+                            painter.drawLine(center + QPoint(0, -8), center + QPoint(3, -5));
+                            break;
+                        case EAST:
+                            painter.drawLine(center, center + QPoint(8, 0));
+                            painter.drawLine(center + QPoint(8, 0), center + QPoint(5, -3));
+                            painter.drawLine(center + QPoint(8, 0), center + QPoint(5, 3));
+                            break;
+                        case SOUTH:
+                            painter.drawLine(center, center + QPoint(0, 8));
+                            painter.drawLine(center + QPoint(0, 8), center + QPoint(-3, 5));
+                            painter.drawLine(center + QPoint(0, 8), center + QPoint(3, 5));
+                            break;
+                        case WEST:
+                            painter.drawLine(center, center + QPoint(-8, 0));
+                            painter.drawLine(center + QPoint(-8, 0), center + QPoint(-5, -3));
+                            painter.drawLine(center + QPoint(-8, 0), center + QPoint(-5, 3));
+                            break;
+                        default:
+                            break;
+                    }
+                } else if (map[x][y].type == INTERSECTION) {
+                    painter.fillRect(rect, QColor(128, 128, 128)); // Light gray for intersections
+                    
+                    // Draw intersection symbol
+                    painter.setPen(QPen(Qt::white, 3));
+                    QPoint center = rect.center();
+                    painter.drawLine(center + QPoint(-8, 0), center + QPoint(8, 0));
+                    painter.drawLine(center + QPoint(0, -8), center + QPoint(0, 8));
+                }
+                
+                // Draw grid lines
+                painter.setPen(QPen(Qt::black, 1));
+                painter.drawRect(rect);
+            }
+        }
+    }
+
+private:
+    vector<vector<MapPoint>> createEmptyMap();
+    void createRoads(vector<vector<MapPoint>>& map);
+};
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    
+    QMainWindow window;
+    MapWidget *mapWidget = new MapWidget();
+    window.setCentralWidget(mapWidget);
+    window.setWindowTitle("City Simulation");
+    window.show();
+    
+    cout << "Qt City Simulation started!" << endl;
+    
+    return app.exec();
 }
 
-vector<vector<MapPoint>> createEmptyMap() {
+vector<vector<MapPoint>> MapWidget::createEmptyMap() {
     vector<vector<MapPoint>> map(MAP_WIDTH, vector<MapPoint>(MAP_HEIGHT));
 
     for (int x = 0; x < MAP_WIDTH; x++) {
@@ -58,7 +143,7 @@ vector<vector<MapPoint>> createEmptyMap() {
     return map;
 }
 
-void createRoads(vector<vector<MapPoint>>& map) {
+void MapWidget::createRoads(vector<vector<MapPoint>>& map) {
     // Create a horizontal road in the middle of the map
     for (int x = 0; x < MAP_WIDTH; x++) {
         map[x][MAP_HEIGHT / 2].type = ROAD;
@@ -88,28 +173,8 @@ void createRoads(vector<vector<MapPoint>>& map) {
     }
 }
 
-void printMap(const vector<vector<MapPoint>>& map) {
-    for (int y = 0; y < MAP_HEIGHT; y++) {
-        for (int x = 0; x < MAP_WIDTH; x++) {
-            string symbol = ".";
-            if (map[x][y].type == ROAD) {
-                if (map[x][y].direction == NORTH) {
-                    symbol = "↑"; // Vertical road
-                } else if (map[x][y].direction == EAST) {
-                    symbol = "→"; // Horizontal road
-                } else if (map[x][y].direction == SOUTH) {
-                    symbol = "↓"; // Vertical road
-                } else if (map[x][y].direction == WEST) {
-                    symbol = "←"; // Horizontal road
-                } 
-            } else if (map[x][y].type == INTERSECTION) {
-                    symbol = "┼"; // Intersection
-                }
-            cout << symbol;
-        }
-        cout << endl;
-    }
-}
+// Include the MOC file for Qt's meta-object system
+#include "main.moc"
 
 
 
